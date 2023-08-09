efef519a7101c2d72c9570cd408e805a66aea87c
Beatriz Prado Lopes <90909819+BEA32@users.noreply.github.com>
2023-08-09 20:26:31 -0300

    Aula 01

---
+++ Aula_01/exemplo1.ts
@@ -0,0 +1,16 @@
+namespace exemplo1 {
+    let numero: number = 4;
+    let numero2: number = 10;
+
+    let result: number;
+    result = numero + numero2
+    console.log("A soma de " + numero + " com "
+    + numero2 + " é igual a: " + result);   
+    result = numero - numero2;
+    console.log(`A subtração de ${numero} com ${numero2} é igual a ${result}`);
+    result = numero * numero2;
+    console.log(`A multiplicação ${numero} com ${numero2} é igual a ${result}`);
+     result = numero / numero2;
+     console.log(`A divisão ${numero} com ${numero2} é igual a ${result}`);
+    
+}
---
+++ Aula_01/exemplo2.ts
@@ -0,0 +1,30 @@
+namespace exemplo2 {
+    let idade: number = 18;
+
+    if(idade >= 18)
+    {
+        console.log("A pessoa pode beber");
+    } else {
+        console.log("A pessoa não pode beber");
+        
+    }
+
+    let resultado: string
+    resultado = idade >= 18 ? "Pode beber" : "Não pode beber";
+    console.log(resultado);
+    
+    switch (idade) {
+        case 18:
+            console.log("Pode beber");
+            break;
+        case 15:
+            console.log("Pode beber refri");
+            break;
+        case 7:
+            console.log("Pode beber suco");
+            break;
+        default:
+            console.log("Beba água");
+            break;
+    }
+}
